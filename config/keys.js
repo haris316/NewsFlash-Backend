2803af9b8f9dbef3059f540179def57f67b5a429
haris316 <gogeto931@gmail.com>
2022-02-02 16:49:31 +0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://root:qwertyshiba123@shibainu.jnhhl.mongodb.net/shibainudb?retryWrites=true&w=majority",
+    secretOrKey: 'superconfidentialrandomsecretdonotshareshushhhh',
+    nodemaileruser: "shibaccepted@gmail.com",
+    nodemailerpass: "qwerty@shibainu##"
+}
